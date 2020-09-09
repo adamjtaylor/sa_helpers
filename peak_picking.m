05e311a397bf5029f25d7c90563113be879c8bf8
Adam Taylor <adamjtaylor@gmail.com>
2020-09-09 21:37:16 +0100

    Update peak_picking.m

--- peak_picking.m
+++ peak_picking.m
@@ -1,4 +1,4 @@
-function peak_picking(input_file, sa_path)
+function peak_picking(input_file, nzm_multiple, sa_path)
 
 %% Input is a mat file containing spectralChannels and intensities vectors
 
@@ -10,7 +10,11 @@ load(input_file);
 
 spectrum = SpectralData(spectralChannels, intensities)
 
-medianPeakFilter = PeakThresholdFilterMedian(1, 3);
+peakPicking = GradientPeakDetection();
+medianPeakFilter = PeakThresholdFilterMedian(1, nzm_multiple);
+peakPicking.addPeakFilter(medianPeakFilter);
+
+
 peakPicking.addPeakFilter(medianPeakFilter);
 
 peaks = peakPicking.process(spectrum);
