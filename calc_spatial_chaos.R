1c4d71cd4765d598d15876293ee6096762323a76
Adam Taylor <adamjtaylor@gmail.com>
2020-09-16 13:43:19 +0100

    Update calc_spatial_chaos.R

--- calc_spatial_chaos.R
+++ calc_spatial_chaos.R
@@ -1,36 +1,44 @@
+#' Calculate spatial chaos
+#'
+#' @param input_file A -v7.3 .mat file output from the adamjtaylor/nextflow_msi pipeline.
+#'
+#' @return A csv file containing mz values, index, tb_ratio and spatial chaos for peaks with tb_ratio > 0
+#' @export
+#'
+#' @examples
 calc_spatial_chaos <- function(input_file) {
-
-library(tidyverse)
-library(hdf5r)
-library(SPUTNIK)
-
-h5_file <- hdf5r::h5file(input_file, mode = "r")
-
-pixels <- h5_file[["pixels"]]$read() %>% as_tibble() %>% rename(x=V1, y = V2)
-mz <- h5_file[["spectralChannels"]]$read()
-tb_ratio <- h5_file[["tb_ratio"]]$read()
-datacube <- h5_file[["data"]]$read()
-
-h5_file$close_all()
-
-max_x <- max(pixels$x)
-max_y <- max(pixels$y)
-
-calc_s_chaos <- function(index){
-  datacube[,index] %>% matrix(max_y, max_x) %>% spatial.chaos()
-}
-
-tibble(
-  mz = mz,
-  index = 1:length(mz),
-  tb_ratio = tb_ratio
+  
+  library(tidyverse)
+  library(hdf5r)
+  library(SPUTNIK)
+  
+  h5_file <- hdf5r::h5file(input_file, mode = "r")
+  
+  pixels <- h5_file[["pixels"]]$read() %>% as_tibble() %>% rename(x=V1, y = V2)
+  mz <- h5_file[["spectralChannels"]]$read()
+  tb_ratio <- h5_file[["tb_ratio"]]$read()
+  datacube <- h5_file[["data"]]$read()
+  
+  h5_file$close_all()
+  
+  max_x <- max(pixels$x)
+  max_y <- max(pixels$y)
+  
+  calc_s_chaos <- function(index){
+    datacube[,index] %>% matrix(max_y, max_x) %>% spatial.chaos()
+  }
+  
+  tibble(
+    mz = mz,
+    index = 1:length(mz),
+    tb_ratio = tb_ratio
   ) %>%
-  filter(tb_ratio > 0) %>%
-  mutate(
-    s_chaos = map_dbl(index, calc_s_chaos)
+    filter(tb_ratio > 0) %>%
+    mutate(
+      s_chaos = map_dbl(index, calc_s_chaos)
     ) %>%
-  write_csv("spatial_chaos_tissue.csv")
-
-h5_file$close_all()
+    write_csv("spatial_chaos_tissue.csv")
+  
+  h5_file$close_all()
   
 }
